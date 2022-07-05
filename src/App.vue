f84bb5c7fe6cb418c85114a6cdd936a1fc49e19e
sugianto43 <antho9922@gmail.com>
2022-07-05 19:12:55 +0800

    create UI component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,70 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- HEADER COMPONENT -->
+    <div class="header-title">
+      <p class="label">Diari Jajan Bulan Februari</p>
+      <p>Pengeluaran bulan ini Rp 5.605.475</p>
+
+      <b-button variant="primary" @click="showModal = !showModal"
+        >TAMBAH ITEM</b-button
+      >
+
+      <!-- MODAL ADD ITEM -->
+      <b-modal v-model="showModal" hide-footer hide-header centered class="p-5">
+        <h4>Tambah Entri</h4>
+        <br />
+
+        <label for="name">Nama</label>
+        <b-form-input id="name" type="text" v-model="name" />
+
+        <br />
+
+        <label for="price">Harga</label>
+        <b-form-input id="price" type="number" v-model="price" />
+
+        <div class="btn-wrapper">
+          <b-button class="cancel-btn" @click="handleCancelAdd">BATAL</b-button>
+          <b-button variant="primary">KIRIM</b-button>
+        </div>
+      </b-modal>
+      <!-- END OF MODAL ADD ITEM -->
+    </div>
+    <!-- END OF HEADER COMPONENT -->
+
+    <!-- CONTENT -->
+    <div class="content-wrapper">
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+      <CardItem />
+    </div>
+    <!--END OF CONTENT -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import CardItem from "./components/CardItem.vue";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    CardItem,
+  },
+  data() {
+    return {
+      showModal: false,
+      name: "",
+      price: null,
+    };
+  },
+  methods: {
+    handleCancelAdd() {
+      this.showModal = false;
+      this.name = "";
+      this.price = null;
+    },
+  },
+};
 </script>
 
 <style>
@@ -21,8 +72,45 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.header-title {
+  text-align: center;
+  padding: 30px;
+}
+
+.modal-body {
+  padding: 20px !important;
+}
+
+.label {
+  font-size: 20px;
+  font-weight: bold;
+  margin-bottom: 0;
+}
+
+.btn-wrapper {
+  display: flex;
+  justify-content: flex-end;
+  margin-top: 20px;
+}
+
+.cancel-btn {
+  margin-right: 15px;
+}
+
+.content-wrapper {
+  display: grid;
+  padding: 40px 30px;
+  width: 100%;
+  max-width: 1250px;
+  margin: 0 auto;
+  grid-template-columns: repeat(auto-fill, minmax(320px, 1fr));
+  grid-auto-rows: auto;
+  grid-gap: 1rem;
 }
 </style>
---
+++ src/components/CardItem.vue
@@ -0,0 +1,77 @@
+<template>
+  <b-card class="shadow-sm">
+    <h6 class="date">14 Februari</h6>
+    <div class="card-item">
+      <div class="time">09:20</div>
+      <div class="title">Ayam Goreng</div>
+      <div class="price">Rp 25.000</div>
+    </div>
+
+    <div class="card-item">
+      <div class="time">09:20</div>
+      <div class="title">Ayam Goreng</div>
+      <div class="price">Rp 25.000</div>
+    </div>
+
+    <div class="card-item">
+      <div class="time">09:20</div>
+      <div class="title">Ayam Goreng</div>
+      <div class="price">Rp 25.000</div>
+    </div>
+
+    <div class="total-price">
+      <p>Total</p>
+      <p class="value-price">Rp 544.987</p>
+    </div>
+  </b-card>
+</template>
+
+<script>
+export default {
+  name: "CardItem",
+  props: {},
+};
+</script>
+
+<style scoped>
+.card-item {
+  display: flex;
+  align-items: center;
+  border-top: 1px solid rgba(128, 128, 128, 0.2);
+  padding: 10px;
+}
+
+.date {
+  font-weight: bold;
+}
+
+.time {
+  width: 15%;
+  font-size: 14px;
+}
+
+.title {
+  width: 60%;
+  font-size: 14px;
+  padding-left: 10px;
+}
+
+.price {
+  width: 25%;
+  text-align: right;
+  font-size: 14px;
+}
+
+.total-price {
+  display: flex;
+  justify-content: flex-end;
+  border-top: 2px solid rgba(128, 128, 128, 0.3);
+  font-weight: bold;
+  padding: 15px 10px 0 10px;
+  font-size: 14px;
+}
+
+.value-price {
+  margin-left: 40px;
+}
+</style>
